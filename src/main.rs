76893bae1e1cef9e43bd8b44a3d54faa086589b8
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-11-08 21:20:15 -0600

    Init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use dioxus::prelude::*;
+
+fn main() {
+    console_error_panic_hook::set_once();
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        div {
+            style: "text-align: center;",
+            h1 { "🌗 Dioxus wow 🚀" }
+            h3 { "Frontend that scales." }
+            p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
+        }
+    ))
+}
