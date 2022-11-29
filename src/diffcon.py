fe7cc1e4521cdab8bfb112f2df11c4361d40dd4b
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2022-11-29 04:15:02 -0600

    Diff & Con Method

---
+++ src/diffcon.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+import numpy as np
+
+def calculare_diff(img1: str, img2: str, ext: str, out_dir: str):
+    img1 = cv.imread(rf"{img1}")
+    img2 = cv.imread(rf"{img2}")
+
+    # Resizing the images to max 500 pixels
+    img1 = cv.resize(img1, (500, 500))
+    img2 = cv.resize(img2, (500, 500))
+
+    # Convert to grayscale and apply Gaussian blur
+    img1_gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+    img1_blur = cv.GaussianBlur(img1_gray, (5, 5), 0)
+
+    img2_gray = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+    img2_blur = cv.GaussianBlur(img2_gray, (5, 5), 0)
+
+    # Binary thresholding of the images
+    ret, img1_thresh = cv.threshold(img1_blur, 127, 255, cv.THRESH_BINARY)
+    ret, img2_thresh = cv.threshold(img2_blur, 127, 255, cv.THRESH_BINARY)
+
+    # Get difference between images
+    img_diff = cv.absdiff(img1_thresh, img2_thresh)
+
+    # Reduce noise
+    kernel = np.ones((8, 2), np.uint8)
+    img_diff = cv.morphologyEx(img_diff, cv.MORPH_OPEN, kernel)
+
+    # Dilate the image
+    img_diff = cv.dilate(img_diff, kernel, iterations=5)
+
+    # Show connected components in the image
+    num_labels, labels, stats, centroids = cv.connectedComponentsWithStats(img_diff)
+
+    # Draw the bounding boxes of 
+    for i in range(1, num_labels):
+        x = stats[i, cv.CC_STAT_LEFT]
+        y = stats[i, cv.CC_STAT_TOP]
+        w = stats[i, cv.CC_STAT_WIDTH]
+        h = stats[i, cv.CC_STAT_HEIGHT]
+        cv.rectangle(img1, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    path = rf"{out_dir}/img.{ext}"
+    cv.imwrite(path, img1)
+
+    return (num_labels - 1, f"{path}")
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use dioxus_router::*;
 use pyo3::Python;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
+use anyhow::Result;
 
 mod icons;
 use icons::{MoonIcon, SunIcon};
@@ -48,7 +49,8 @@ pub fn Sticky(cx: Scope) -> Element {
                     class:"flex items-center justify-center text-sm space-x-10 text-white",
                     ItemStickyMenu { to: "/credit", "Créditos" }
                     ItemStickyMenu { to: "/howto", "¿Cómo funciona?" }
-                    ItemStickyMenu { to: "/", "App" }
+                    ItemStickyMenu { to: "/haar", "Haar Cascade" }
+                    ItemStickyMenu { to: "/", "Diff & Connect" }
                     div {
                         "onclick": "{SCRIPT}",
                         class: "cursor-pointer hover:text-gray-200",
@@ -152,11 +154,203 @@ def transform_image(x, y):
     }
 }
 
+fn diff_n_conn(img1: &str, img2: &str, ext: &str, save_in: &str) -> Result<(i32, String)> {
+    let result = Python::with_gil(|py| {
+        let script = PyModule::from_code(py, 
+            include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/diffcon.py")),  
+            "diffcon.py", 
+            "diffcon"
+        )?;
+
+    let relu_result: (i32, String) = script.getattr("calculare_diff")?.call1((img1, img2, ext, save_in))?.extract()?;
+    println!("Result: {:?}", relu_result);
+        
+    Ok::<(i32 , String), anyhow::Error>(relu_result)
+    });
+
+    if let Ok(result) = result {
+        Ok(result)
+    } else {
+        Err(result.unwrap_err())
+    }
+}
+
 #[inline_props]
-fn MainApp(cx: Scope) -> Element {
+fn DiffMethod(cx: Scope) -> Element {
+    let base64_image: &UseState<String> = use_state(&cx, || "".to_owned());
+    let base64_image_ready: &UseState<bool> = use_state(&cx, || false);
+    let cars_in_image: &UseState::<i32> = use_state(&cx, || 0);
+
+    let placeholder_path_1: &UseState<String> = use_state(&cx, || directories::UserDirs::new().unwrap().home_dir().to_str().unwrap().to_owned());
+    let valid_path_1: &UseState<String> = use_state(&cx, || "".to_owned());
+
+    let placeholder_path_2: &UseState<String> = use_state(&cx, || directories::UserDirs::new().unwrap().home_dir().to_str().unwrap().to_owned());
+    let valid_path_2: &UseState<String> = use_state(&cx, || "".to_owned());
+
+    cx.render(rsx! {
+        Main {
+            footer: false,
+            div {
+                class: "flex flex-col items-center justify-center",
+                h1 {
+                    class: "font-sans font-thin mb-5 text-xl",
+                    "Diff & Connect Method"
+                }
+                div {
+                    class: "w-4/5",
+                    ///// Placeholder 1
+                    div{
+                        class: "flex items-center justify-center",
+                        input {
+                            class: "bg-neutral-200 dark:bg-titlebar text-dark dark:text-white rounded-md p-2 w-4/5",
+                            "type": "text",
+                            value: "{placeholder_path_1}",
+                            oninput: move |evt| {
+                                let value = &evt.value.trim();
+                                placeholder_path_1.set(evt.value.to_owned());
+                                let path =  std::path::PathBuf::from_str(value).unwrap();
+                                if path.exists() && !path.is_dir() {
+                                    valid_path_1.set(path.to_str().unwrap().to_owned());
+                                }
+    
+                            },
+                        }
+                        button {
+                            class: "bg-neutral-200 dark:bg-titlebar text-dark dark:text-white rounded-md p-2 ml-2 w-1/5",
+                            "type": "button",
+                            onclick: |_| {
+                                let path = rfd::FileDialog::new()
+                                .add_filter("image", &["png", "jpg", "jpeg"])
+                                .set_directory(directories::UserDirs::new().unwrap().home_dir().to_str().unwrap())
+                                .pick_file();
+    
+                                if let Some(path) = path {
+                                    placeholder_path_1.set(path.to_str().unwrap().to_owned());
+                                    valid_path_1.set(path.to_str().unwrap().to_owned());
+                                }
+                            },
+                            "Browse"
+                        }
+                    }
+                    ///// Placeholder 2
+                    div{
+                        class: "flex items-center justify-center mt-2",
+                        input {
+                            class: "bg-neutral-200 dark:bg-titlebar text-dark dark:text-white rounded-md p-2 w-4/5",
+                            "type": "text",
+                            value: "{placeholder_path_2}",
+                            oninput: move |evt| {
+                                let value = &evt.value.trim();
+                                placeholder_path_2.set(evt.value.to_owned());
+                                let path =  std::path::PathBuf::from_str(value).unwrap();
+                                if path.exists() && !path.is_dir() {
+                                    valid_path_2.set(path.to_str().unwrap().to_owned());
+                                }
+    
+                            },
+                        }
+                        button {
+                            class: "bg-neutral-200 dark:bg-titlebar text-dark dark:text-white rounded-md p-2 ml-2 w-1/5",
+                            "type": "button",
+                            onclick: |_| {
+                                let path = rfd::FileDialog::new()
+                                .add_filter("image", &["png", "jpg", "jpeg"])
+                                .set_directory(directories::UserDirs::new().unwrap().home_dir().to_str().unwrap())
+                                .pick_file();
+    
+                                if let Some(path) = path {
+                                    placeholder_path_2.set(path.to_str().unwrap().to_owned());
+                                    valid_path_2.set(path.to_str().unwrap().to_owned());
+                                }
+                            },
+                            "Browse"
+                        }
+                    }
+                    div {
+                        class: "flex justify-center items-center",
+                        button {
+                            class: "bg-neutral-200 dark:bg-titlebar text-dark dark:text-white rounded-md p-2 mt-2 w-full",
+                            onclick: |_| {
+                                let data_dir = directories::ProjectDirs::from("com", "up", "imp").unwrap();
+                                let data_dir = data_dir.data_dir();
+    
+                                if !data_dir.exists() {
+                                    std::fs::create_dir_all(data_dir).unwrap();
+                                }
+                               
+                                let path1 = std::path::PathBuf::from_str(valid_path_1.get()).unwrap();
+                                let path2 = std::path::PathBuf::from_str(valid_path_2.get()).unwrap();
+                                if !path1.exists() || path1.is_dir() || !path2.exists() || path2.is_dir() {
+                                    base64_image_ready.set(false);
+                                    return;
+                                }
+
+                                let img_extension_1 = std::path::PathBuf::from_str( valid_path_1.get()).unwrap();
+                                let img_extension_1 = img_extension_1.extension().unwrap().to_str().unwrap();
+                                let new_path_1 = data_dir.join(format!("old_img_1.{img_extension_1}"));
+
+                                let img_extension_2 = std::path::PathBuf::from_str( valid_path_2.get()).unwrap();
+                                let img_extension_2 = img_extension_2.extension().unwrap().to_str().unwrap();
+                                let new_path_2 = data_dir.join(format!("old_img_2.{img_extension_2}"));
+
+                                println!("Copying file to {:?} from {:?}", new_path_1, valid_path_1.get());
+                                std::fs::copy(valid_path_1.get(), new_path_1.to_str().unwrap()).unwrap();
+
+                                println!("Copying file to {:?} from {:?}", new_path_2, valid_path_2.get());
+                                std::fs::copy(valid_path_2.get(), new_path_2.to_str().unwrap()).unwrap();
+    
+                                let result = diff_n_conn(
+                                    new_path_1.to_str().unwrap(),
+                                    new_path_2.to_str().unwrap(),
+                                    img_extension_1,
+                                    data_dir.to_str().unwrap()
+                                );
+                                
+                                if let Ok(result) = result {
+                                    let path = std::path::PathBuf::from_str(&result.1).unwrap();
+                                    println!("Set state to {}",path.display());
+                                    let mut file: std::fs::File = std::fs::OpenOptions::new()
+                                        .read(true).open(path).unwrap();
+                                    let mut contents = vec![];
+                                    file.read_to_end(&mut contents).unwrap();
+                                    base64_image.set(base64::encode(&contents));
+                                    cars_in_image.set(result.0);
+                                    base64_image_ready.set(true);
+                                } else {
+                                    base64_image_ready.set(false);
+                                    println!("Error: {:?}", result.unwrap_err());
+                                }
+                            },
+                            "Do it!"
+                        }
+                    }
+                    div {
+                        class: "flex justify-center items-center mt-5",
+                        div {
+                            class: "flex flex-col items-center",
+                            base64_image_ready.then(|| rsx! {
+                                p {
+                                    class: "text-center",
+                                    "There are {cars_in_image} cars in the image!"
+                                }
+                                img { 
+                                    class: "mt-2 w-1/2",
+                                    src: "data:image/png;base64,{base64_image}" 
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
+
+#[inline_props]
+fn HaarMethod(cx: Scope) -> Element {
     let state: &UseState<String> = use_state(&cx, || "".to_owned());
     let spath: &UseState<String> = use_state(&cx, || {
-        format!("{}", std::env::current_dir().unwrap().to_str().unwrap())
+        format!("{}", directories::UserDirs::new().unwrap().home_dir().to_str().unwrap())
     });
     let spath_valid: &UseState<String> = use_state(&cx, || "".to_owned());
     let state_img: &UseState<bool> = use_state(&cx, || false);
@@ -167,8 +361,8 @@ fn MainApp(cx: Scope) -> Element {
             div {
                 class: "flex flex-col items-center justify-center",
                 h1 {
-                    class: "font-sans font-thin mb-5",
-                    "Dioxus wooo"
+                    class: "font-sans font-thin mb-5 text-xl",
+                    "Haar Cascade Method"
                 }
                 div {
                     class: "w-4/5",
@@ -301,7 +495,8 @@ fn Credit(cx: Scope) -> Element {
 fn app(cx: Scope) -> Element {
     cx.render(rsx!(
         Router {
-            Route { to: "/", MainApp {} }
+            Route { to: "/", DiffMethod {} }
+            Route { to: "/haar", HaarMethod {} }
             Route { to: "/howto", Howto {} }
             Route { to: "/credit", Credit {} }
         }
